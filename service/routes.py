7f33d53ff0012a5b122e2dd744b5fbb286693a00
John Rofrano <johnnyroy@johnrofrano.com>
2021-10-13 02:47:08 +0000

    Initial load

---
+++ service/routes.py
@@ -0,0 +1,132 @@
+######################################################################
+# Copyright 2016, 2021 John J. Rofrano. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+######################################################################
+
+"""
+Module for Hit Counter Service Routes
+"""
+
+import os
+from redis import Redis
+from flask import jsonify, url_for, abort
+from werkzeug.exceptions import NotFound
+from redis.exceptions import ConnectionError
+from . import status
+from service import app
+
+# Connext to the Redis database
+counter = None
+if 'DATABASE_URI' in os.environ:
+    DATABASE_URI = os.getenv("DATABASE_URI")
+    counter = Redis.from_url(DATABASE_URI, encoding="utf-8", decode_responses=True)
+else:
+    REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+    REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
+    counter = Redis(host=REDIS_HOST, port=REDIS_PORT, encoding="utf-8", decode_responses=True)
+
+
+############################################################
+# Index page
+############################################################
+@app.route("/")
+def index():
+    app.logger.info("Request for Base URL")
+    return jsonify(
+        status=status.HTTP_200_OK,
+        message="Hit Counter Service",
+        version="1.0.0",
+        url=url_for("list_counters", _external=True),
+    )
+
+
+############################################################
+# List counters
+############################################################
+@app.route("/counters", methods=["GET"])
+def list_counters():
+    app.logger.info("Request to list all counters...")
+    counters = [
+        dict(name=key, counter=int(counter.get(key))) for key in counter.keys("*")
+    ]
+    return jsonify(counters)
+
+
+############################################################
+# Create counters
+############################################################
+@app.route("/counters/<name>", methods=["POST"])
+def create_counters(name):
+    app.logger.info("Request to Create counter...")
+    count = counter.get(name)
+    if count is not None:
+        return jsonify(code=409, error="Counter already exists"), 409
+
+    counter.set(name, 0)
+
+    location_url = url_for("read_counters", name=name, _external=True)
+    return (
+        jsonify(name=name, counter=0),
+        status.HTTP_201_CREATED,
+        {"Location": location_url},
+    )
+
+
+############################################################
+# Read counters
+############################################################
+@app.route("/counters/<name>", methods=["GET"])
+def read_counters(name):
+    app.logger.info("Request to Read counter...")
+    count = counter.get(name)
+    if count is None:
+         raise NotFound(f"Counter {name} does not exist")
+
+    return jsonify(name=name, counter=int(count))
+
+
+############################################################
+# Update counters
+############################################################
+@app.route("/counters/<name>", methods=["PUT"])
+def update_counters(name):
+    app.logger.info("Request to Update counter...")
+    count = counter.get(name)
+    if count is None:
+        raise NotFound(f"Counter {name} does not exist")
+
+    count = counter.incr(name)
+    return jsonify(name=name, counter=count)
+
+
+############################################################
+# Delete counters
+############################################################
+@app.route("/counters/<name>", methods=["DELETE"])
+def delete_counters(name):
+    app.logger.info("Request to Delete counter...")
+    count = counter.get(name)
+    if count is not None:
+        counter.delete(name)
+
+    return "", status.HTTP_204_NO_CONTENT
+
+
+############################################################
+# Utility for testing
+############################################################
+def reset_counters():
+    global counter
+    if app.testing:
+        counter.flushall()
---
+++ tests/test_service.py
@@ -0,0 +1,127 @@
+"""
+Counter API Service Test Suite
+
+Test cases can be run with the following:
+  nosetests -v --with-spec --spec-color
+  coverage report -m
+"""
+import os
+import logging
+from unittest import TestCase
+from service import status  # HTTP Status Codes
+from service.log_handler import initialize_logging
+from service.routes import app, reset_counters
+
+
+######################################################################
+#  T E S T   C A S E S
+######################################################################
+class CounterTest(TestCase):
+    """ REST API Server Tests """
+
+    @classmethod
+    def setUpClass(cls):
+        """ This runs once before the entire test suite """
+        app.testing = True
+        initialize_logging(logging.CRITICAL)
+
+    @classmethod
+    def tearDownClass(cls):
+        """ This runs once after the entire test suite """
+        pass
+
+    def setUp(self):
+        """ This runs before each test """
+        reset_counters()
+        self.app = app.test_client()
+
+    def tearDown(self):
+        """ This runs after each test """
+        pass
+
+######################################################################
+#  T E S T   C A S E S 
+######################################################################
+
+    def test_index(self):
+        """ Test index call """
+        resp = self.app.get("/")
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+    def test_create_counters(self):
+        """ Test Create a counter """
+        name = "foo"
+        resp = self.app.post(f"/counters/{name}")
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        data = resp.get_json()
+        self.assertEqual(data["name"], name)
+        self.assertEqual(data["counter"], 0)
+
+    def test_list_counters(self):
+        """ Test List counters """
+        resp = self.app.get("/counters")
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 0)
+        # create a counter and name sure it appears in the list
+        self.app.post("/counters/foo")
+        self.app.post("/counters/bar")
+        self.app.post("/counters/baz")
+        resp = self.app.get("/counters")
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 3)
+
+    def test_read_counters(self):
+        """ Test Read a counter """
+        name = "foo"
+        self.test_create_counters()
+        resp = self.app.get(f"/counters/{name}")
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], name)
+        self.assertEqual(data["counter"], 0)
+
+    def test_update_counters(self):
+        """ Test Update a counter """
+        name = "foo"
+        self.test_create_counters()
+        resp = self.app.get(f"/counters/{name}")
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        print(data)
+        self.assertEqual(data["name"], name)
+        self.assertEqual(data["counter"], 0)
+        # now update it
+        resp = self.app.put(f"/counters/{name}")
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], name)
+        self.assertEqual(data["counter"], 1)
+
+    def test_delete_counters(self):
+        """ Test Delete a counter """
+        name = "foo"
+        self.test_create_counters()
+        resp = self.app.delete(f"/counters/{name}")
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+        resp = self.app.get(f"/counters/{name}")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_method_not_allowed(self):
+        """ Test method not allowed """
+        resp = self.app.post(f"/counters")
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_counter_already_exists(self):
+        """ Test counter already exists """
+        name = "foo"
+        self.test_create_counters()
+        resp = self.app.post(f"/counters/{name}")
+        self.assertEqual(resp.status_code, status.HTTP_409_CONFLICT)
+
+    def test_update_not_found(self):
+        """ Test update not found """
+        name = "foo"
+        resp = self.app.put(f"/counters/{name}")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
